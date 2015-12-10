ee7ee041bb77fee7b2cf399375d59d73240307c1
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2015-12-10 22:00:20 +0100

    Updated GETSTARTED.md

--- app/screens/get-started/index.js
+++ app/screens/get-started/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const getstarted = require('../../../docs/GETSTARTED.md');
-
 export default class GetStarted extends React.Component {
     render() {
         return (
             <div className="wrapper">
-                <div className="markdown-body" dangerouslySetInnerHTML={{__html: getstarted}} />
+                <div
+                    className="markdown-body"
+                    dangerouslySetInnerHTML={{__html: require('../../../docs/GETSTARTED.md')}}
+                />
             </div>
         );
     }
