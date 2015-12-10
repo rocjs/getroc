b94a7e25f9679667f950fdb5f730ac02dc0b1edb
Greger Stolt Nilsen <gregersn@gmail.com>
2015-12-10 15:33:04 +0100

    start component for highlight element

---
+++ app/components/highlights/highlight/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import styles from './style.scss';
+
+export default class Highlight extends React.Component {
+    static propTypes = {
+        image: React.PropTypes.string.isRequired
+    }
+    render() {
+        return(
+            <div className={styles.highlight}>
+                <div className={(this.props.image in styles) ? styles[this.props.image] : styles.image} >
+                </div>
+                <div>
+                    { this.props.children }
+                </div>
+            </div>
+        );
+    }
+}
