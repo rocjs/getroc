693998ce6e618c6683e86dc82a06c5694a6c4b52
Andreas Søvik <andreas.sovik@vg.no>
2015-12-10 18:35:04 +0100

    Moved markdown to docs. Add getstarted markdown to view.

--- app/screens/get-started/index.js
+++ app/screens/get-started/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-const hero = require('../../../public/HERO.md');
+const hero = require('../../../docs/GETSTARTED.md');
 
 export default class GetStarted extends React.Component {
     render() {
         return (
             <div className="wrapper">
-                <h1>Get started</h1>
-                <p>
-                    Get started components here!
-                </p>
                 <div className="markdown-body" dangerouslySetInnerHTML={{__html: hero}} />
             </div>
         );
