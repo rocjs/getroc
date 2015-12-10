1f03a16181e20314db45664f2ff356ffced4ec2e
Andreas Søvik <andreas.sovik@vg.no>
2015-12-10 09:52:40 +0100

    Project base using roc-web-react

---
+++ roc.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    config: {
+        applicationName: 'getroc.org - get started developing today!',
+        port: 3000,
+        build: {
+            reducers: 'reducers.js',
+            routes: 'routes.js'
+        }
+    }
+};
