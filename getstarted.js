2146a7ff984be7709da2f68a493c221d39965cdf
Andreas Søvik <andreas.sovik@vg.no>
2015-12-10 11:32:52 +0100

    Add base routes. Index and get started components.

---
+++ getstarted.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styles from './style.scss';
+
+export default class GetStarted extends React.Component {
+    render() {
+        return (
+            <div className={styles.main}>
+                <h1>Get started</h1>
+                <p>
+                    Get started components here!
+                </p>
+            </div>
+        );
+    }
+}
