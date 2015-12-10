e643e3d9e04df6224e728562e1311b76a112f50c
Greger Stolt Nilsen <gregersn@gmail.com>
2015-12-10 19:21:37 +0100

    added text with highlights, and some temporary icons

--- app/components/highlights/highlight/index.js
+++ app/components/highlights/highlight/index.js
@@ -3,15 +3,15 @@ import React from 'react';
 import styles from './style.scss';
 
 export default class Highlight extends React.Component {
-    static propTypes = {
-        image: React.PropTypes.string.isRequired
-    }
+    // static propTypes = {
+    //     image: React.PropTypes.string.isRequired
+    // }
     render() {
         return(
             <div className={styles.highlight}>
                 <div className={(this.props.image in styles) ? styles[this.props.image] : styles.image} >
                 </div>
-                <div>
+                <div className={styles.text}>
                     { this.props.children }
                 </div>
             </div>
