cf11668b7c855388e844b2a7ca428523259e383f
Andreas Søvik <andreas.sovik@vg.no>
2016-08-03 22:07:59 +0200

    Update roc config to work with the 'next' set of changes.

--- roc.config.action.js
+++ roc.config.action.js
@@ -1,18 +1,20 @@
 // we can make a plugin for this
-module.exports = () => (roc) => {
-    const hook = roc.hook;
+module.exports = {
+    action: (roc) => {
+        const hook = roc.hook;
 
-    if (hook === 'build-webpack') {
-        // the return value from the previous action on this hook
-        const rocBuilder = roc.previousValue;
+        if (hook === 'build-webpack') {
+            // the return value from the previous action on this hook
+            const webpack = roc.previousValue;
 
-        return () => () => {
-            // add markdown loader
-            rocBuilder.buildConfig.module.loaders.push(
-                { test: /\.md$/, loader: 'html!markdown' }
-            );
+            return () => () => {
+                // add markdown loader
+                webpack.module.loaders.push(
+                    { test: /\.md$/, loader: 'html!markdown' }
+                );
 
-            return rocBuilder;
-        };
+                return webpack;
+            };
+        }
     }
 };
--- roc.config.js
+++ roc.config.js
@@ -5,19 +5,20 @@ module.exports = {
             port: 3000,
             serve: ['public', 'build/client'],
             favicon: 'favicon.png',
-            link: [],
             template: {
                 path: 'templates'
             }
         },
         build: {
             resources: ['roc-package-web-app-react/styles/base.css', 'app/styles/markdown.scss'],
-            reducers: 'app/redux/reducers.js',
-            reduxMiddlewares: 'app/redux/middlewares.js',
-            routes: 'app/routes/index.js'
+            routes: 'app/routes/index.js',
+            redux: {
+                reducers: 'app/redux/reducers.js',
+                middlewares: 'app/redux/middlewares.js'
+            }
         }
     },
     // an action lets us hook right into any important Roc context
     // in this app we just want to add markdown loader to webpack
-    action: require('./roc.config.action')
+    actions: [require('./roc.config.action')]
 };
