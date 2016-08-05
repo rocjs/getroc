b67dab44956672ecab620d2a2abd26b11000614a
Andreas Søvik <andreas.sovik@vg.no>
2016-08-05 13:14:13 +0200

    Rename file and default export

--- app/screens/docs/index.js
+++ app/screens/docs/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class GetStarted extends React.Component {
+export default class Docs extends React.Component {
     render() {
         return (
             <div className="wrapper">
